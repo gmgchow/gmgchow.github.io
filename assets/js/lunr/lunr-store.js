3ae0d1547611001f9236ca3d809ee7e132ef263b
gmgchow <gmgchow@users.noreply.github.com>
2022-07-30 06:32:11 +0000

    Upload site built from commit 3e7ad8437f8b60feee178d603a2562f72fd5282f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Welcome to my new blog! I’ve been wanting to make a personal website and blog since forever but somehow I never found the time for it. Currently, I’m on vacation back in Vancouver so I finally found some time to get started! The view from the Vancouver Public Library rooftop...","categories": [],
+        "tags": ["random"],
+        "url": "/blog/2022/06/22/hello-world.html",
+        "teaser": null
+      },{
+        "title": "I graduated from Mercari!",
+        "excerpt":"Last month, I resigned from Mercari where I worked for almost five years! Mercari is a marketplace app similar to Craigslist or Ebay. I joined Mercari on November 1st, 2017. It was my first job in Japan and since I was uncertain about my future, I joined with the intention...","categories": [],
+        "tags": ["mercari","career"],
+        "url": "/blog/2022/07/14/mercari-retrospective.html",
+        "teaser": null
+      }]
---
+++ zh/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"歡迎來到我的新博客！   其實我一直都想做一個個人網站和博客，但是找不到時間做。現在我在溫哥華放假所以終於有點多餘時間做了！    在溫哥華公共圖書館天臺看到的風景（溫哥華Downtown，加拿大）   我打算寫關於我有興趣的話題的作文（tech行業，cybersecurity，心理學，事業和自我完善，日本的生活，等等）。   後日就快開放的作文：     我在最近辭職的公司Mercari的時間的retrospective   人生第一次從兩家公司拿offer，談判工資的經驗   最近看完，想推薦的書   現在，這個網站在用premade的設計所以相當不好看。我打算就快做點進步：     吧UI設計改善一下   加日文和中文的翻譯（警告：因爲我想練習一下這兩個語言，我打算自己翻譯。有可能會翻譯得很奇怪，抱歉。。。）   在profile理留了我的Twitter和LinkedIn，如果有comment，特別想我寫的話題，意見，提案等等，可以隨便聯絡我。   謝謝你的閲讀，希望能在下一個post再見！    我在享受溫哥華的美味點心！很久不吃正宗的中式菜了！  ","categories": [],
+        "tags": ["隨意"],
+        "url": "/blog/2022/06/22/hello-world.html",
+        "teaser": null
+      },{
+        "title": "我從Mercari畢業了!",
+        "excerpt":"上個月，剛剛從我工作差不多5年的Mercari畢業了！ Mercari是一個像Craigslist或者Ebay的網購app. 我2017年11月1日加入了Mercari。是我在日本的第一份工，當時不知道會留在日本多長時間所以打算暫時留一年半兩年左右。完全沒想過會留在差不多5年啊！在公司經歷了4年9個月，6月14日，終於去了辦公室最後的一次。 感覺上好像走了很長的路，成長了很多。能説加入Mercari時候的我跟現在的我已經是兩個不同的人！作爲這幾年的retrospective，我寫了這篇文章。 時間綫 2017年11月 - 2018年10月: SET和自動化測試時代 當初我是作爲負責做自動化測試的Software in Test (SET)加入Mercari的。 當時用的技術棧： iOS: XCUITest, Swift, Earl Grey Android: Ruby, Appium API: JMeter, Postman 主要的成果： 把iOS自動化測試加入了development repository和CI build pipeline 建立了iOS nightly build 改善了自動化測試後果報告的format 作了一個 fastlane plu gin，把它open-source 2018年10月 - 2019年7月: Testdeck時代 Mercari開始轉去microservice architecture的時候，我就加入了爲了作一個microservice自動化測試的社内tool的新project。我們作的叫 Testdeck，後期把它open-source了。我以前在 這篇文章介紹了一下。 主要的成果： 爲了參加這個project和後期做Testdeck的lead，獨立學習了microservices，Kubernetes，GCP，Golang等等 跟microservice工程師們一起工作。幫助他們導入自動化測試和troubleshoot，還有收集了關於Testdeck的feature request和改善了Testdeck...","categories": [],
+        "tags": ["mercari","事業"],
+        "url": "/blog/2022/07/14/mercari-retrospective.html",
+        "teaser": null
+      }]
