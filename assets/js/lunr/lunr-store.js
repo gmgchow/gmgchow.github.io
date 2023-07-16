a514fd43a29e22c537230a0ce0d28561a9f64085
gmgchow <gmgchow@users.noreply.github.com>
2023-07-16 13:59:50 +0000

    Upload site built from commit 3a3ddc8f68a5aa085668ea2956f5f24c251de1f7

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"(EN/JA/ZH) Welcome to my new blog! 日本語で読む 看中文版 I’ve been wanting to make a personal website and blog since forever but somehow I never found the time for it. Currently, I’m on vacation back in Vancouver so I finally found some time to get started! The view from the Vancouver...","categories": [],
+        "tags": ["random"],
+        "url": "/blog/2022/06/22/hello-world.html",
+        "teaser": null
+      },{
+        "title": "I graduated from Mercari!",
+        "excerpt":"(EN/JA/ZH) Last month, I resigned from Mercari where I worked for almost five years! 日本語で読む 看中文版 Mercari is a marketplace app similar to Craigslist or Ebay. I joined Mercari on November 1st, 2017. It was my first job in Japan and since I was uncertain about my future, I joined...","categories": [],
+        "tags": ["mercari","career","job change"],
+        "url": "/blog/2022/07/14/mercari-retrospective.html",
+        "teaser": null
+      },{
+        "title": "Six Signs A Company Has Good Work-Life Balance",
+        "excerpt":"(EN/JA) Recently, work-life balance has been something that I’ve been thinking about a lot because things at work have been busy and sometimes it feels like my personal goals have taken a backseat. I wrote this article to share my thoughts on why work-life balance is important and how to...","categories": [],
+        "tags": ["career"],
+        "url": "/blog/2022/10/02/work-life-balance.html",
+        "teaser": null
+      },{
+        "title": "What Does Probation Period Really Mean?",
+        "excerpt":"(EN/JA) This month, I officially finished my three-month probation period at Japan Computer Vision. This is my third company- therefore my third time going through probation- but it was the first time that I actually stopped to think about what probation period really means, for both me and the company....","categories": [],
+        "tags": ["career","job change"],
+        "url": "/blog/2022/10/24/probation-period.html",
+        "teaser": null
+      },{
+        "title": "I passed my PNPT Exam!",
+        "excerpt":"(EN/JA) Last week, I passed the PNPT (Practical Network Penetration Tester) exam! This certification is hands-down, the best that I have taken so far but it is still fairly unknown in the industry. I decided to write this article to share my experience and hopefully help spread word about this...","categories": [],
+        "tags": ["cybersecurity","career","certifications","studying"],
+        "url": "/blog/2022/12/05/PNPT.html",
+        "teaser": null
+      },{
+        "title": "Farewell, Japan Computer Vision!",
+        "excerpt":"(EN/JA/ZH) Last week was my last working day at Japan Computer Vision (JCV), a Softbank subsidiary that develops facial recognition AI products. I joined in July 2022, exactly one year ago. 日本語で読む 看中文版 Japan Computer Vision is a subsidiary of Softbank Group. Life at JCV What I did As the...","categories": [],
+        "tags": ["softbank","jcv","career","job change"],
+        "url": "/blog/2023/07/16/jcv.html",
+        "teaser": null
+      },{
+        "title": "English Books",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/english",
+        "teaser": null
+      },{
+        "title": "Reading List",
+        "excerpt":"Reviews of books that I read.   WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list",
+        "teaser": null
+      },{
+        "title": "日本の書籍",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/japanese",
+        "teaser": null
+      },{
+        "title": "To-Read",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/to-read",
+        "teaser": null
+      }]
---
+++ zh/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"(EN/JP/ZH) 歡迎來到我的新博客！      Read in English   日本語で読む   其實我一直都想做一個個人網站和博客，但是找不到時間做。現在我在溫哥華放假所以終於有點多餘時間做了！    在溫哥華公共圖書館天臺看到的風景（溫哥華Downtown，加拿大）   我打算寫關於我有興趣的話題的作文（tech行業，cybersecurity，心理學，事業和自我完善，日本的生活，等等）。   後日就快開放的作文：     我在最近辭職的公司Mercari的時間的retrospective   人生第一次從兩家公司拿offer，談判工資的經驗   最近看完，想推薦的書   現在，這個網站在用premade的設計所以相當不好看。我打算就快做點進步：     吧UI設計改善一下   加日文和中文的翻譯（警告：因爲我想練習一下這兩個語言，我打算自己翻譯。有可能會翻譯得很奇怪，抱歉。。。）   在profile理留了我的Twitter和LinkedIn，如果有comment，特別想我寫的話題，意見，提案等等，可以隨便聯絡我。   謝謝你的閲讀，希望能在下一個post再見！    我在享受溫哥華的美味點心！很久不吃正宗的中式菜了！  ","categories": [],
+        "tags": ["隨意"],
+        "url": "/blog/2022/06/22/hello-world.html",
+        "teaser": null
+      },{
+        "title": "我從Mercari畢業了!",
+        "excerpt":"(EN/JA/ZH) 上個月，剛剛從我工作差不多5年的Mercari畢業了！ Read in English 日本語で読む Mercari是一個像Craigslist或者Ebay的網購app. 我2017年11月1日加入了Mercari。是我在日本的第一份工，當時不知道會留在日本多長時間所以打算暫時留一年半兩年左右。完全沒想過會留在差不多5年啊！在公司經歷了4年9個月，6月14日，終於去了辦公室最後的一次。 感覺上好像走了很長的路，成長了很多。能説加入Mercari時候的我跟現在的我已經是兩個不同的人！作爲這幾年的retrospective，我寫了這篇文章。 時間綫 2017年11月 - 2018年10月: SET和自動化測試時代 當初我是作爲負責做自動化測試的Software in Test (SET)加入Mercari的。 當時用的技術棧： iOS: XCUITest, Swift, Earl Grey Android: Ruby, Appium API: JMeter, Postman 主要的成果： 把iOS自動化測試加入了development repository和CI build pipeline 建立了iOS nightly build 改善了自動化測試後果報告的format 作了一個 fastlane plu gin，把它open-source 2018年10月 - 2019年7月: Testdeck時代 Mercari開始轉去microservice architecture的時候，我就加入了爲了作一個microservice自動化測試的社内tool的新project。我們作的叫 Testdeck，後期把它open-source了。我以前在...","categories": [],
+        "tags": ["mercari","事業"],
+        "url": "/blog/2022/07/14/mercari-retrospective.html",
+        "teaser": null
+      },{
+        "title": "Six Signs A Company Has Good Work-Life Balance",
+        "excerpt":"(EN/JA) Recently, work-life balance has been something that I’ve been thinking about a lot because things at work have been busy and sometimes it feels like my personal goals have taken a backseat. I wrote this article to share my thoughts on why work-life balance is important and how to...","categories": [],
+        "tags": ["career"],
+        "url": "/blog/2022/10/02/work-life-balance.html",
+        "teaser": null
+      },{
+        "title": "What Does Probation Period Really Mean?",
+        "excerpt":"(EN/JA) This month, I officially finished my three-month probation period at Japan Computer Vision. This is my third company- therefore my third time going through probation- but it was the first time that I actually stopped to think about what probation period really means, for both me and the company....","categories": [],
+        "tags": ["career","job change"],
+        "url": "/blog/2022/10/24/probation-period.html",
+        "teaser": null
+      },{
+        "title": "I passed my PNPT Exam!",
+        "excerpt":"(EN/JA) Last week, I passed the PNPT (Practical Network Penetration Tester) exam! This certification is hands-down, the best that I have taken so far but it is still fairly unknown in the industry. I decided to write this article to share my experience and hopefully help spread word about this...","categories": [],
+        "tags": ["cybersecurity","career","certifications","studying"],
+        "url": "/blog/2022/12/05/PNPT.html",
+        "teaser": null
+      },{
+        "title": "再見，Japan Computer Vision！",
+        "excerpt":"(EN/JA/ZH) 上週是我在 Japan Computer Vision (JCV) 的最後的一天。JCV是Softbank集團的AI startup，專門開發人臉識別人工智慧產品。我是去年的7月加入的，工作了正好一年。 Read in English 日本語で読む JCV是Softbank集團的子公司。 在JCV的生活 工作内容 作爲Product Team裏邊的第一位兼唯一的Security Engineer，我主要負責建立application security的過程，將SAST/DAST整合到開發流程中，還有做網站、mobile app、API、IoT產品的penetration test。 Softbank總部 Tokyo Portcity Takeshiba, Softbank集團的總部所在地 JCV的工程師大概是full-remote的，所以我很少去了辦公室，但辦公室在Softbank集團的HQ，Tokyo Portcity Takeshiba。這是一座很漂亮而相當新建的辦公大樓，外面種了很多植物，裏面有high-tech清潔機器人，從很多角度能看到海景，還有餐廳，便利店等等的設備！ 一個安保機器人在辦公樓裏巡。 清潔機器人們在休息中。它們大概在營業時間外活動，以免打擾人。 我個人非常喜歡Softbank的員工食堂（不知道爲什麽，但它被暱稱為“Cafeshiba”），差不多每次去辦公室都會去那裡吃飯。大部分的人好像會選擇咖哩飯、烏冬等等的日式餐，但我總是點cafe的芝士三明治！那裡還有各種甜點，比較適合我的口味。 我在Softbank食堂最喜歡點的午餐！ 當然，有時候我也會在那裡工作。除了午飯晚飯的rush以外，其實相當安靜，而且海景非常美麗。 從食堂看到的東京灣美麗景色。 食堂的食物不是免費的，但價格還算便宜（大約500日元可以得到一餐）。 回憶 在三語環境中工作 對我來説，在JCV工作最有趣的是，工作環境使用三種語言：英文，中文（普通話）和日文！ 將來我應該沒有機會去中國或台灣工作，所以這可能是我第一次而最後一次在工作中使用中文，成爲了一個非常有意義的經驗。當初我覺得很難跟上（老實説現在也還是…），但是日常參加中文的會議，閲讀完成用中文寫的Slack channel等等，的確讓我中文進步很多了。今年3月，我居然考上了漢語水平考試HSK6級，非常感謝有機會在這樣diverse的環境中工作！ 我HSK6級的點數是正好合格的。如果沒有JCV的同事們跟我練習中文，覺得肯定不能考上了。 當然，不是公司每個人都會三種語言。有些人只會一種，有些人會一種半，而有些人是三種語言都完成流利的。我自己只是英文流利的，還在學習中文和日語，所以對三種語言都流利的同事們真的充滿敬意。 玩轉人臉識別設備 親愛的讀者，您在日本有見過這種設備嗎？ JCV的SenseThunder溫度檢測相機在疫情期間，在日本各地的購物商場、電影院、餐廳等等經常能看到的。 在JCV工作的一個有趣的方面是，有機會參與研發這個設備和其他可以在日本看到的人臉識別產品，所以覺得自己的工作在社會中是可見的，對社會有貢獻的。每次我在公共地方看見JCV產品的時候都忍不住想告訴人：“你看，這是我公司做的！是我負責做security的！” 享受WeWork生活 對我個人來說，我在JCV最喜歡的福利是員工可以在任何WeWork辦公地點工作。一直在家裏工作的話，有時候覺得很無聊，也有時候下班後有東西要做，所以在東京的中心地區（新宿、澀谷等等）工作比較方便。那時候，我使用了其他WeWork辦公地點。 其實，在JCV的任職期間中，我去了東京和橫濱的不少WeWork辦公地點，甚至還去了大阪的一個地點啊！...","categories": [],
+        "tags": ["softbank","jcv","事業","轉工"],
+        "url": "/blog/2023/07/16/jcv.html",
+        "teaser": null
+      },{
+        "title": "English Books",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/english",
+        "teaser": null
+      },{
+        "title": "Reading List",
+        "excerpt":"Reviews of books that I read.   WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list",
+        "teaser": null
+      },{
+        "title": "日本の書籍",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/japanese",
+        "teaser": null
+      },{
+        "title": "To-Read",
+        "excerpt":"WIP  ","categories": [],
+        "tags": [],
+        "url": "/reading-list/to-read",
+        "teaser": null
+      }]
